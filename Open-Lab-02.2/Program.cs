114b8e2b4b4f17ee948c23259740649280f05cf7
romankalab <roan.kalab@gmail.com>
2019-10-14 09:32:24 +0200

    Initial

---
+++ Open-Lab-02.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace skuška
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            float[] array1 = new float[] { 1.154f, 32.5f, 515.2456f, 7.1254f, 91.254f };
+            float count = 0;
+            foreach (float element in array1)
+            {
+                count++;
+                Console.Write($"{element.ToString("N2")} ");
+            }
+
+        }
+
+
+
+    }
+}
